0fbf00da8b3b70003253b4eb562fbfec1c3a2523
Jim Nguyen <dragonoath123@gmail.com>
2019-04-29 00:26:58 -0700

    added notes to help with my project

--- Lab8/recursion.c
+++ Lab8/recursion.c
@@ -16,3 +16,19 @@ int main(){
     printf("%d\n", recursion(x));
     return 0;
 }
+
+
+
+
+int price, exchange, allowance
+    5          1
+if(price < allowance){ if allowance is more than price thats good 
+
+}
+else{
+
+}
+
+money / price
+4         2
+2         4
